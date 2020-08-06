47efc035657e86605d9baaf2d05da81326bc9f4d
VaughanJackson123 <vjackson@companieshouse.gov.uk>
2020-08-06 16:21:40 +0100

    GCI-1312: Test transformer error propagation.

---
+++ transformer/transformer_test.go
@@ -0,0 +1,28 @@
+package transformer
+
+import (
+	"github.com/companieshouse/payment-reconciliation-consumer/data"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func TestUnitErrorHandling1(t *testing.T) {
+
+	// This test happens to work correctly because the path defaults to the current directory.
+	Convey("GetEshuResources propagates product_codes.yml file path  error", t, func() {
+
+		// Given
+		transformerUnderTest := Transform{}
+
+		// When
+		_, err := transformerUnderTest.GetEshuResources(
+			data.PaymentResponse{},
+			data.PaymentDetailsResponse{},
+			"paymentId string")
+
+		// Then
+		So(err, ShouldNotBeNil)
+
+	})
+
+}
