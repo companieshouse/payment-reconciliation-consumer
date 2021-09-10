929320d7d2ab62be146d103e2ca610b971935f5f
Michael Acreman <macreman.ch@gmail.com>
2021-09-10 10:53:05 +0100

    untested example of logic required to filter out payments that do not have a valid product code mapping - code not run - unit tests undoubtedly broken - for reference only

--- data/paymentResponse.go
+++ data/paymentResponse.go
@@ -2,6 +2,9 @@ package data
 
 import (
 	"time"
+
+	"github.com/companieshouse/chs.go/log"
+	"github.com/companieshouse/payment-reconciliation-consumer/config"
 )
 
 const DataMaintenance = "data-maintenance"
@@ -62,7 +65,28 @@ type RefundResource struct {
 }
 
 // Indicates whether the payment is reconcilable or not.
-func (payment PaymentResponse) IsReconcilable() bool {
-	return (payment.Costs[0].ClassOfPayment[0] == DataMaintenance ||
-		payment.Costs[0].ClassOfPayment[0] == OrderableItem)
+func (payment PaymentResponse) IsReconcilable(productMap *config.ProductMap) bool {
+	// return (payment.Costs[0].ClassOfPayment[0] == DataMaintenance ||
+	// 	payment.Costs[0].ClassOfPayment[0] == OrderableItem)
+
+	classOfPayment := payment.Costs[0].ClassOfPayment[0]
+
+	// only reconcile these payment classes, others like penalty and legacy reconcile elsewhere
+	if (classOfPayment == DataMaintenance || classOfPayment == OrderableItem) {
+
+		productType := payment.Costs[0].ProductType
+		productCode := productMap.Codes[productType]
+
+		// if product code is zero there is no mapping for the product type so the payment is not reconcilable in CHS
+		if (productCode == 0) {
+			log.Info("Not reconcilable due to product code", log.Data{"reference":payment.Reference, "class_of_payment": classOfPayment, "product_type": productType, "product_code": productCode})
+			return false
+		}
+
+		log.Info("Reconcilable payment", log.Data{"reference":payment.Reference, "class_of_payment": classOfPayment, "product_type": productType, "product_code": productCode})
+		return true
+	}
+
+	log.Info("Not reconcilable due to class of payment", log.Data{"reference":payment.Reference, "class_of_payment": classOfPayment})
+	return false
 }
